929472f70f43c2028ddc6230b84be63d92a1d6d3
IAmJamesHall <thatwhichendureth@gmail.com>
2020-04-09 17:22:12 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
-// function App() {
-//   axios.get('http://localhost:5000/api/courses')
-//     .then(courses => courses.data.map(course => <li>course.title</li>))
-//     .then(courseTitles => {
-//       return (
-//         <div className="App">
-//           <ul>
-//             <li>{courseTitles}</li>
-//           </ul>
-//         </div>
-//       )
-//     })
-//     .catch(err => console.log('ERROR: ', err))
-// }
-
 class App extends Component {
   state = {
     courses: []
